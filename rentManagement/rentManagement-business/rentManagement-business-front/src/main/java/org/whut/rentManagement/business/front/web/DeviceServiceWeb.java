4ff2612d49922d98703b57434b3dea1e9876e072
aaronletoo <aaronletoo@gmail.com>
2014-12-30 15:45:51 +0800

    ...

---
+++ rentManagement/rentManagement-business/rentManagement-business-front/src/main/java/org/whut/rentManagement/business/front/web/DeviceServiceWeb.java
@@ -0,0 +1,11 @@
+package org.whut.rentManagement.business.front.web;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Aaron
+ * Date: 14-12-30
+ * Time: 下午3:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class DeviceServiceWeb {
+}
